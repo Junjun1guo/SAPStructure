613ff302927864f9c71a69e27184ee6b41249fff
Junjun Guo <jjguo2@bjtu.edu.cn>
2025-08-28 15:20:38 +0800

    Add files via upload

--- example-3-modelMesh/modelMesh_pygmsh.py
+++ example-3-modelMesh/modelMesh_pygmsh.py
@@ -14,7 +14,11 @@ import pygmsh
 import pyvista as pv
 import matplotlib.pyplot as plt
 import sys
-sys.path.append("..")
+########################################################################################################################
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.abspath(os.path.join(current_dir, '..'))
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
 ########################################################################################################################
 ########################################################################################################################
 from auxiliaryModules.mainMod import EleMeshPlotAndSelect
